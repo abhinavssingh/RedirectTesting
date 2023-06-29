58f9de8fd2450fc3f72759529ed9b2a6ad50ac81
Abhinav Singh <Abhinav.Singh@ghd.com>
2023-06-29 15:06:43 +0530

    Add project files.

---
+++ RedirectTesting/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedirectTesting
+{
+    public class Program
+    {
+
+        static Task Main()
+        {
+            string strFilePath = @"C:\RedirectTesting\RedirectTest.csv";
+            List<string> outputLlist = new List<string>();
+
+            using (StreamReader reader = new StreamReader(strFilePath))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    var response = RedirectUrlAsync(line);
+                    if (response != null)
+                    {
+                        Console.WriteLine($"Requested url is {line},status code = {(int)response.Result.StatusCode},Redirected url is {response.Result.RequestMessage.RequestUri}");
+                        outputLlist.Add($"Requested url is {line},status code = {(int)response.Result.StatusCode},Redirected url is {response.Result.RequestMessage.RequestUri}");
+                    }
+                }
+            }
+
+            string docPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, "ResponseRedirect.txt")))
+            {
+                foreach (string output in outputLlist)
+                {
+                    outputFile.WriteLine(output);
+                }
+
+            }
+
+            return Task.CompletedTask;
+        }
+
+        static async Task<HttpResponseMessage> RedirectUrlAsync(string line)
+        {
+            var client = new HttpClient();
+            var requestUri = new Uri(line);
+            var response = await client.GetAsync(requestUri);
+            return response;
+        }
+    }
+}
